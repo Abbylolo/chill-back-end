18c16bc3a7e390084161853a9cdaabe86bc9024e
Abbylolo <2542111098@qq.com>
2023-05-09 16:36:01 +0800

    pref:修改logger内容

--- app/logger.js
+++ app/logger.js
@@ -1,17 +1,44 @@
 const { createLogger, format, transports } = require("winston");
+const fs = require("fs");
+const path = require("path");
 
-module.exports = createLogger({
+const env = process.env.NODE_ENV || "development";
+const logDir = "log";
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const filename = path.join(logDir, "results.log");
+
+const logger = createLogger({
+  // change level if in dev environment versus production
+  level: env === "production" ? "info" : "debug",
+  format: format.combine(
+    format.label({ label: path.basename(process.mainModule.filename) }),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" })
+  ),
   transports: [
-    new transports.File({
-      filename: "logs/server.log",
-      level: "info",
+    new transports.Console({
       format: format.combine(
-        format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
-        format.align(),
+        format.colorize(),
         format.printf(
-          (info) => `${info.level}: ${[info.timestamp]}: ${info.message}`
+          (info) =>
+            `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+        )
+      ),
+    }),
+    new transports.File({
+      filename,
+      format: format.combine(
+        format.printf(
+          (info) =>
+            `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
         )
       ),
     }),
   ],
 });
+
+module.exports = logger;
